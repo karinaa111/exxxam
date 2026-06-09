89806646fc45481f9598981a23c0a7104ba5bdbd
heavy <karinaa01011vvvv@gmail.com>
2026-06-09 20:47:06 +0300

    Update settings.py

--- branches_project/settings.py
+++ branches_project/settings.py
@@ -8,6 +8,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.auth",
+    "django.contrib.staticfiles",
     "rest_framework",
     "drf_yasg",
     "branches_app",
